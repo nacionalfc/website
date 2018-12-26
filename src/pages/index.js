950e29204ad1cc5e4a08da460fad3376effea47a
Ruan Carvalho <ruancarvalho@gmail.com>
2018-12-25 22:55:25 -0400

    chore(pages): changed home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,45 +7,24 @@ const IndexPage = () => (
     <article>
       <header className="entry-header">
         <h1 className="entry-title" style={{ 'text-align': 'center' }}>
-          Convocação do Conselho Deliberativo
+          FELIZ NATAL E PRÓSPERO ANO NOVO
         </h1>
         <div className="entry-meta" style={{ 'text-align': 'center' }}>
           <span className="posted-on">
-            <time className="entry-date published" datetime="2018-11-30T18:22:42+00:00">
-              30 de novembro de 2018
-            </time>
+            &nbsp;
           </span>
         </div>
       </header>
 
       <div className="entry-content">
         <p style={{'text-align':'center'}}><strong>&nbsp;</strong></p>
-        <p style={{'text-align':'center'}}><strong>&nbsp;Edital de Convocação do Conselho Deliberativo</strong></p>
-        <p style={{'text-align':'center'}}><strong>&nbsp;</strong></p>
-        <p>O Presidente do Conselho Deliberativo, no uso de suas atribuições estatutárias, convoca os senhores conselheiros,
-          a comparecerem no dia 06 de Dezembro de 2018, em sua sede social (salão de troféus), sito à Rua São Luís, nº 230 (altos),
-          bairro Adrianópolis, nesta cidade de Manaus/AM, as 18h30 em primeira convocação e às 19h30 em segunda convocação com
-      qualquer número de conselheiros para tratarem da seguinte ordem do dia:</p>
-        <p>&nbsp;</p>
-        <ol>
-          <li>Eleição do presidente e vice-presidentes da diretoria executiva, conforme art. 22- II 1º.</li>
-          <li>As chapas de inscrição deverão constar o nome do presidente e dos vice-presidentes e devem ser registrados na secretaria
-        do clube até 18 horas do dia 05/12/2018 (quarta-feira).</li>
-        </ol>
-        <p>&nbsp;</p>
-        <p>O modelo de inscrição das chapas e outras informações relativas ao pleito eleitoral serão fixados na secretaria do
-      clube.</p>
-        <p>&nbsp;</p>
-        <p>NORMAS PARA AS ELEIÇÕES:</p>
-        <p><a href="https://nacionalfc1913.files.wordpress.com/2018/11/normas-para-eleic3a7c3a3o.docx">DOWNLOAD: NORMAS PARA ELEIÇÃO (.docx)</a></p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <p>Manaus, 28 de Novembro de 2018.</p>
-        <p>&nbsp;</p>
-        <p><strong>Sidney Guilherme</strong></p>
-        <p>Presidente do Conselho Deliberativo</p>
-        <p>&nbsp;</p>
-        <p>&nbsp;</p>
+        <p>
+          O Nacional Futebol Clube deseja a todos os seus diretores,
+          conselheiros, funcionários, colaboradores e torcedores e seus 
+          familiares, um Feliz Natal e Próspero Ano Novo. Que possamos 
+          juntos celebrar a chegada de mais um ano, com harmonia e paz!
+        </p>
+        <p>Boas Festas!</p>        
       </div>
     </article>
   </Layout>
